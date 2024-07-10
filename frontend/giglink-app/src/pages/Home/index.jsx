bb1d1e705e0095001c168a6a757407500a8e2e13
Абужапар Гасанбеков <abusikgasanbekov@MacBook-Air-Abuzapar.local>
2024-07-10 20:25:55 +0300

    create react app

---
+++ frontend/giglink-app/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
---
+++ frontend/giglink-app/src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css'
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
