d766d41b4d8c4341d8e54fb76d25891bea28c0dc
lom-ali <lomali1999999@mail.ru>
2024-07-11 12:48:43 +0300

    проверка

--- frontend/giglink-app/src/App.jsx
+++ frontend/giglink-app/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
+import Down from '../src/components/Down'
 
 function App() {
   return (
     <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/' element={< Down/>}/>
+
     </Routes>
   )
 }
